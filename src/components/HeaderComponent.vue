a0428e82e80a0f63c8969c5dbf232657388be680
Aryans <quispeloayza18@hotmail.com>
2024-03-18 15:56:32 -0500

    definiendo strore

--- src/components/HeaderComponent.vue
+++ src/components/HeaderComponent.vue
@@ -1,9 +1,12 @@
 <script setup lang="ts">
 import { useRoute } from "vue-router";
 import { computed } from 'vue';
+import { useBebidasStore } from "@/stores/bebidas";
 const route = useRoute()
 
 const paginaInicio = computed(() => route.name === 'inicio')
+const store = useBebidasStore()
+console.log(store.categorias)
 
 
 </script>
