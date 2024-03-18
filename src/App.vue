db6127badac59491c08eae7db9d1c940619cfb26
Aryans <quispeloayza18@hotmail.com>
2024-03-18 15:44:01 -0500

    edit Inicio

--- src/App.vue
+++ src/App.vue
@@ -1,10 +1,18 @@
 <script setup lang="ts">
+import { RouterView } from 'vue-router';
 import Header from './components/HeaderComponent.vue';
 </script>
 
 <template>
 
+
   <Header>
 
   </Header>
-</template>./components/HeaderComponent.vue
+
+  <main class="container mx-auto mt-10 ">
+    <RouterView></RouterView>
+  </main>
+
+  
+</template>
